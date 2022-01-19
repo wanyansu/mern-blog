44680b31ef30d94bd0da50456aad07f2310d4979
wanyansu <wanyansuroy@gmail.com>
2022-01-19 14:11:34 +1000

    my fourth attempt

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -3,7 +3,7 @@ import Header from "../../components/header/Header";
 import Posts from "../../components/posts/Posts";
 import Sidebar from "../../components/sidebar/Sidebar";
 import "./home.css";
-import axiosInstance from "../../config";
+import axiosInstance from "../../config.js";
 import { useLocation } from "react-router";
 
 export default function Home() {
